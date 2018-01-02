f80744a49c5c2bfa8f1177e8f29abafb77499d36
Codebender <tawanda@mmogodigital.com>
2018-01-02 10:35:32 +0200

    Changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var Stratum = require('stratum-pool');
 
 var pool = Stratum.createPool({
-  "coin": 'Dash',
+  "coin": Dash,
 
   "address": 'Xu7PWxCzQrQBWKHjBCrM4r469ceY1GiEj4',
   "blockRefreshInterval": 1000,
